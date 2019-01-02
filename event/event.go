0d46200e8e35fdce5485f97cf3a517314d478a7e
Swetha Repakula <srepaku@us.ibm.com>
2019-01-02 14:30:24 -0800

    [FAB-13102] Update burrow to 0.23.3
     - burrow update has breaking changes requiring a refactor of the evm
     - statemanager operations now match burrow expectations
     - evmcc calls to statemanager go through evmCache
     - evmCache must create accounts before manipulating account data
     - contract account permissions are being handled by the evmcc directly
     and no longer need to be handled in the state manager.
     - sequence numbers are handled by evmcache and are stored on the
     useraccount. user accounts that deploy contracts are now stored in
     the ledger
     - account.Address has been changed to crypto.Address due to refactoring
     in burrow project
    
     - This commit also removes event support which will be addressed in a
     upcoming commit
    
    Change-Id: I917214dfa1d97b7b3e77c85d93bb6449f9cef02e
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

--- event/event.go
+++ event/event.go
@@ -8,50 +8,48 @@ package event
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
-	"github.com/hyperledger/burrow/execution/evm/events"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 type EventManager struct {
-	stub       shim.ChaincodeStubInterface
-	EventCache []events.EventDataLog
+	stub shim.ChaincodeStubInterface
+	// EventCache []events.EventDataLog
 }
 
 func NewEventManager(stub shim.ChaincodeStubInterface) *EventManager {
 	return &EventManager{
-		stub:       stub,
-		EventCache: []events.EventDataLog{},
+		stub: stub,
+		// EventCache: []events.EventDataLog{},
 	}
 }
 
 func (evmgr *EventManager) Flush(eventName string) error {
-	if len(evmgr.EventCache) == 0 {
-		return nil
-	}
-	payload, err := json.Marshal(evmgr.EventCache)
-	if err != nil {
-		return fmt.Errorf("Failed to marshal event messages: %s", err.Error())
-	}
-	return evmgr.stub.SetEvent(eventName, payload)
+	// if len(evmgr.EventCache) == 0 {
+	// 	return nil
+	// }
+	// payload, err := json.Marshal(evmgr.EventCache)
+	// if err != nil {
+	// 	return fmt.Errorf("Failed to marshal event messages: %s", err.Error())
+	// }
+	// return evmgr.stub.SetEvent(eventName, payload)
+	return nil
 }
 
 func (evmgr *EventManager) Publish(ctx context.Context, message interface{}, tags map[string]interface{}) error {
-	evID, ok := tags["EventID"].(string)
-	if !ok {
-		return fmt.Errorf("type mismatch: expected string but received %T", tags["EventID"])
-	}
+	// evID, ok := tags["EventID"].(string)
+	// if !ok {
+	// 	return fmt.Errorf("type mismatch: expected string but received %T", tags["EventID"])
+	// }
 
-	msg, ok := message.(*events.EventDataLog)
-	if !ok {
-		return fmt.Errorf("type mismatch: expected *events.EventDataLog but received %T", message)
-	}
+	//msg, ok := message.(*events.EventDataLog)
+	//if !ok {
+	//	return fmt.Errorf("type mismatch: expected *events.EventDataLog but received %T", message)
+	//}
 
-	//Burrow EVM emits other events related to state (such as account call) as well, but we are only interested in log events
-	if evID[0:3] == "Log" {
-		evmgr.EventCache = append(evmgr.EventCache, *msg)
-	}
+	////Burrow EVM emits other events related to state (such as account call) as well, but we are only interested in log events
+	//if evID[0:3] == "Log" {
+	//	evmgr.EventCache = append(evmgr.EventCache, *msg)
+	//}
 	return nil
 }
